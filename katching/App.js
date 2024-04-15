cb6f1d8a92fe40903fbb2c21c85a7d809717dc78
Yuhyun Chung <tonyyhchung@gmail.com>
2024-04-14 17:27:25 -0700

    feat: add web dependency

--- katching/App.js
+++ katching/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Platform } from 'react-native';
 import * as React from "react";
 import BirthdayPage from "./screens/BirthdayPage";
 import IdolGroupSelect from "./screens/IdolGroupSelect";
@@ -8,7 +9,15 @@ import UserNamePage from "./screens/UserNamePage";
 
 const Stack = createStackNavigator();
 
+const os = Platform.OS;
+
+// Get the operating system version
+const version = Platform.Version;
+
 export default function App() {
+  // Log the information
+  console.log('Operating System:', os);
+  console.log('Operating System Version:', version);
   return (
     <NavigationContainer>
       <Stack.Navigator>
